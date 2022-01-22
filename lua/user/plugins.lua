81d050d150413857ea26e7dc96f1ad9259f88cd7
dubravko2108 <dubravkotomic2108@gmail.com>
2022-01-22 10:24:49 +0100

    installing vim-solidity plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -70,6 +70,7 @@ return packer.startup(function(use)
   -- LSP
   use "neovim/nvim-lspconfig" -- enable LSP
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
+  use "tomlion/vim-solidity"
 
   -- Telescope
   use "nvim-telescope/telescope.nvim"
