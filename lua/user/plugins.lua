5d902c1f99a7daad74bc27ac68b76b283bfcd27d
dubravko2108 <dubravkotomic2108@gmail.com>
2022-01-23 03:11:51 +0100

    Add plugin: Null-ls (with format on save)

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -71,6 +71,7 @@ return packer.startup(function(use)
   use "neovim/nvim-lspconfig" -- enable LSP
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
   use "TovarishFin/vim-solidity"
+  use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
 
   -- Telescope
   use "nvim-telescope/telescope.nvim"
--- readme.md
+++ readme.md
@@ -2,3 +2,11 @@
 
 Based on 'LunarVim/Neovim-from-scartch'
 
+## Used LSPs
+solc, pyright, sumneko\_lua, tsserver
+
+## Used linters and formatters
+flake8, prettier, black, stylua
+
+TODO: add solidity to prettier
+
